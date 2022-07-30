e2594e201e25d7b2d5980ceef639b23235c7739d
fawl_03 <razidias@gmail.com>
2022-07-30 18:29:47 -0300

    att 1.1.0 iniciando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.olaClick = this.olaClick.bind(this)
+  }
+  olaClick() {
+    console.log('Funfo')
+  }
+
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
+        <button onClick={this.olaClick}>Olá</button>
       </div>
     );
   }
