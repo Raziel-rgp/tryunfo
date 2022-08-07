e94f021c72d259467f740d8534c775c75ffc4c43
fawl_03 <razidias@gmail.com>
2022-08-07 15:20:11 -0300

    att 1.3.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   state = {
@@ -69,6 +70,7 @@ class App extends React.Component {
           onSaveButtonClick={ this.onSaveButtonClick }
           isSaveButtonDisabled={ isSaveButtonDisabled }
         />
+        <Card { ...this.state } onInputChange={ this.onInputChange } />
       </div>
     );
   }
